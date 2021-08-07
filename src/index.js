5284f7e1f27dea94f5bc3f302ba45436bc0fca4e
Kelly Copley <copleykj@gmail.com>
2021-08-07 08:21:59 -0400

    add reply for testing

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const client = require('./discord-client');
 client.on('ready', () => {
   console.log('client connected and ready');
 });
+
+client.on('messageCreate', message => {
+  if (!message.author.bot && message.content.toLowerCase().includes('hello')) {
+    message.reply('G`Day Mate!');
+  }
+});
